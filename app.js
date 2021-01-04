e2984fa1f4fd5424345901f2a107b31aed58e537
gjaic2016 <gjaic23@gmail.com>
2021-01-04 22:53:43 +0100

    login session cookie, cors

--- app.js
+++ app.js
@@ -6,9 +6,33 @@ const mysql = require("mysql");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 6 * 24,
+    },
+  })
+);
 
 // Create connection
 const db = mysql.createConnection({
@@ -18,7 +42,7 @@ const db = mysql.createConnection({
   database: "quickfix",
 });
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect
@@ -30,9 +54,17 @@ db.connect((err) => {
 });
 
 app.get("/getadds", (req, res) => {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
   let sqlSelect = "SELECT * FROM adds WHERE archive='N'";
   db.query(sqlSelect, (err, result) => {
     console.log(result);
+
     res.send(result);
   });
 });
@@ -59,6 +91,14 @@ app.post("/register", (req, res) => {
   });
 });
 
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
+
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -73,12 +113,14 @@ app.post("/login", (req, res) => {
 
       if (result.length > 0) {
         bcrypt.compare(password, result[0].password, (error, response) => {
-          if(response) {
+          if (response) {
+            req.session.user = result;
+            console.log(req.session.user);
             res.send(result);
           } else {
             res.send({ message: "Pogrešna lozinka/korisničko ime" });
           }
-        })
+        });
       } else {
         res.send({ message: "Korisnik ne postoji" });
       }
@@ -90,4 +132,3 @@ app.listen("3001", () => {
   console.log("Server started, port 3001");
 });
 
-//log
